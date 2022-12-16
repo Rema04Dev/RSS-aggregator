d3f606be8fe1031760c06593bf244b29c957e773
Rema04Dev <romansmerdyakov@gmail.com>
2022-12-16 15:05:33 +0600

    example

--- src/app.js
+++ src/app.js
@@ -1,57 +1,56 @@
-import * as yup from 'yup';
-import i18next from 'i18next';
-import axios from 'axios';
-import watch from './view';
-import resources from './locales/index';
-import parseRSS from './parseRSS';
+import * as yup from "yup";
+import i18next from "i18next";
+import axios from "axios";
+import watch from "./view";
+import resources from "./locales/index";
+import parseRSS from "./parseRSS";
 
-const defaulltLng = 'ru';
+const defaulltLng = "ru";
 
-const validate = (url, urls) => yup
-  .string()
-  .required()
-  .url('mustBeValid')
-  .notOneOf(urls, 'linkExists')
-  .validate(url);
+const validate = (url, urls) =>
+  yup
+    .string()
+    .required()
+    .url("mustBeValid")
+    .notOneOf(urls, "linkExists")
+    .validate(url);
 
-const buildProxyURL = (url) => {
-  const proxiedUrl = new URL('https://allorigins.hexlet.app/get');
-  proxiedUrl.searchParams.set('disableCache', 'true');
-  proxiedUrl.searchParams.set('url', url);
-  return proxiedUrl;
-};
+const buildProxyURL = (url) =>
+  `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(
+    url
+  )}`;
 
 const fetchRSS = (url) => axios.get(buildProxyURL(url));
 
 export default () => {
   const elements = {
-    form: document.querySelector('.rss-form'),
-    input: document.querySelector('#url-input'),
+    form: document.querySelector(".rss-form"),
+    input: document.querySelector("#url-input"),
     button: document.querySelector('[aria-label="add"]'),
 
-    feedback: document.querySelector('.feedback'),
-    posts: document.querySelector('.posts'),
+    feedback: document.querySelector(".feedback"),
+    posts: document.querySelector(".posts"),
 
-    modal: document.querySelector('.modal'),
-    modalTitle: document.querySelector('.modal-title'),
-    modalBody: document.querySelector('.modal-body'),
-    modalLink: document.querySelector('.modal-link'),
-    localesBtnGroup: document.querySelector('.btn-group'),
-    feedsContainer: document.querySelector('.feeds'),
-    postsContainer: document.querySelector('.posts'),
+    modal: document.querySelector(".modal"),
+    modalTitle: document.querySelector(".modal-title"),
+    modalBody: document.querySelector(".modal-body"),
+    modalLink: document.querySelector(".modal-link"),
+    localesBtnGroup: document.querySelector(".btn-group"),
+    feedsContainer: document.querySelector(".feeds"),
+    postsContainer: document.querySelector(".posts"),
 
-    templateFeed: document.querySelector('#template-feeds-wrapper'),
-    templateFeedElement: document.querySelector('#template-feed-element'),
-    templatePost: document.querySelector('#template-posts-wrapper'),
-    templatePostElement: document.querySelector('#template-post-element'),
+    templateFeed: document.querySelector("#template-feeds-wrapper"),
+    templateFeedElement: document.querySelector("#template-feed-element"),
+    templatePost: document.querySelector("#template-posts-wrapper"),
+    templatePostElement: document.querySelector("#template-post-element"),
   };
 
   const initialState = {
     form: {
-      state: 'filling',
+      state: "filling",
       errors: null,
     },
-    lng: 'ru',
+    lng: "ru",
     urls: [],
     feeds: [],
     posts: [],
@@ -67,15 +66,15 @@ export default () => {
   });
 
   const watchedState = watch(initialState, elements, i18nextInstance);
-  elements.form.addEventListener('submit', (evt) => {
+  elements.form.addEventListener("submit", (evt) => {
     evt.preventDefault();
     const formData = new FormData(evt.target);
-    const url = formData.get('url');
+    const url = formData.get("url");
 
     validate(url, watchedState.urls)
       .then((urlRSS) => {
-        watchedState.form.errors = '';
-        watchedState.form.state = 'sending';
+        watchedState.form.errors = "";
+        watchedState.form.state = "sending";
         return fetchRSS(urlRSS);
       })
       .then((response) => {
@@ -83,59 +82,91 @@ export default () => {
         watchedState.feeds.unshift(data.feed);
         watchedState.posts = [...data.posts, ...watchedState.posts];
         watchedState.urls.push(url);
-        watchedState.form.errors = '';
-        watchedState.form.state = 'success';
+        watchedState.form.errors = "";
+        watchedState.form.state = "success";
+        console.log(watchedState);
       })
       .catch((err) => {
-        watchedState.form.state = 'failed';
+        watchedState.form.state = "failed";
         watchedState.form.errors = err.message;
       });
   });
 
-  elements.input.addEventListener('invalid', (evt) => {
+  elements.input.addEventListener("invalid", (evt) => {
     const validityState = evt.target.validity;
     if (validityState.valueMissing) {
-      evt.target.setCustomValidity(i18nextInstance.t('messages.emptyField'));
+      evt.target.setCustomValidity(i18nextInstance.t("messages.emptyField"));
     } else {
-      evt.target.setCustomValidity('');
+      evt.target.setCustomValidity("");
     }
   });
 
-  elements.posts.addEventListener('click', (evt) => {
-    if (evt.target.hasAttribute('data-id')) {
+  elements.posts.addEventListener("click", (evt) => {
+    if (evt.target.hasAttribute("data-id")) {
       const currentPostId = evt.target.dataset.id;
       watchedState.visitedPostsId.push(currentPostId);
     }
 
-    if (evt.target.hasAttribute('data-bs-toggle')) {
+    if (evt.target.hasAttribute("data-bs-toggle")) {
       const { id } = evt.target.dataset;
       watchedState.currentPost = watchedState.posts.find(
-        (post) => post.id === id,
+        (post) => post.id === id
       );
     }
   });
 
-  elements.localesBtnGroup.addEventListener('click', (evt) => {
+  elements.localesBtnGroup.addEventListener("click", (evt) => {
     const language = evt.target.dataset.lng;
     watchedState.lng = language;
   });
 
+  const testUrls = ["https://ru.hexlet.io/lessons.rss"];
+  const testFeeds = [
+    {
+      title: "Новые уроки на Хекслете",
+      description: "Практические уроки по программированию",
+      link: "https://ru.hexlet.io/",
+    },
+  ];
+
+  const testPosts = [
+    {
+      id: "1",
+      title: "Наглядные примеры / Как проектировать классные уроки",
+      description:
+        "Цель: Учимся объяснять абстрактную теорию на примерах из жизни",
+      link: "https://ru.hexlet.io/courses/awesome-text-structure/lessons/examples/theory_unit",
+    },
+    {
+      id: "2",
+      title: "Подходы к объяснению / Как проектировать классные уроки",
+      description:
+        "Цель: Разберем подходы к объяснению теории, которые помогают студентам понять тему",
+      link: "https://ru.hexlet.io/courses/awesome-text-structure/lessons/how-to-explain/theory_unit",
+    },
+    {
+      id: "3",
+      title: "Основные блоки внутри урока / Как проектировать классные уроки",
+      description:
+        "Цель: Изучаем, какими должны быть введение, теория, заключение и дополнительные материалы",
+      link: "https://ru.hexlet.io/courses/awesome-text-structure/lessons/lesson-structure/theory_unit",
+    },
+  ];
   const updatePosts = () => {
-    const { urls } = watchedState;
-    const promises = urls.map((url) => fetchRSS(url))
+      fetchRSS('https://ru.hexlet.io/lessons.rss')
       .then((response) => {
         const updatedData = parseRSS(response.data.contents);
         const newPosts = updatedData.posts;
-        const postLinks = watchedState.posts.map((post) => post.link);
+        const postLinks = testPosts.map((post) => post.link);
         const addedPosts = newPosts.filter(
-          (post) => !postLinks.includes(post.link),
+          (post) => !postLinks.includes(post.link)
         );
-        const updatedPosts = addedPosts.concat(...watchedState.posts);
+        const updatedPosts = addedPosts.concat(...testPosts);
         console.log(updatedPosts);
       })
       .catch((e) => console.log(e.message));
 
-    Promise.all(promises).finally(() => setInterval(() => updatePosts(), 5000));
+    // Promise.all(promises).finally(() => setInterval(() => updatePosts(), 5000));
   };
 
   updatePosts();
