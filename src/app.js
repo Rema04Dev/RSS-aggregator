14b3a17b3a38273d50fd503e1c9558b7c218f9f5
Rema04Dev <romansmerdyakov@gmail.com>
2022-12-16 23:25:29 +0600

    fix updatePosts

--- src/app.js
+++ src/app.js
@@ -1,54 +1,57 @@
-import * as yup from 'yup';
-import i18next from 'i18next';
-import axios from 'axios';
-import watch from './view';
-import resources from './locales/index';
-import parseRSS from './parseRSS';
+import * as yup from "yup";
+import i18next from "i18next";
+import axios from "axios";
+import watch from "./view";
+import _ from "lodash";
+import resources from "./locales/index";
+import parseRSS from "./parseRSS";
 
-const defaulltLng = 'ru';
+const defaulltLng = "ru";
 
-const validate = (url, urls) => yup
-  .string()
-  .required()
-  .url('mustBeValid')
-  .notOneOf(urls, 'linkExists')
-  .validate(url);
+const validate = (url, urls) =>
+  yup
+    .string()
+    .required()
+    .url("mustBeValid")
+    .notOneOf(urls, "linkExists")
+    .validate(url);
 
-const buildProxyURL = (url) => `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(
-  url,
-)}`;
+const buildProxyURL = (url) =>
+  `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(
+    url
+  )}`;
 
 const fetchRSS = (url) => axios.get(buildProxyURL(url));
 
 export default () => {
   const elements = {
-    form: document.querySelector('.rss-form'),
-    input: document.querySelector('#url-input'),
+    form: document.querySelector(".rss-form"),
+    input: document.querySelector("#url-input"),
     button: document.querySelector('[aria-label="add"]'),
 
-    feedback: document.querySelector('.feedback'),
-    posts: document.querySelector('.posts'),
+    feedback: document.querySelector(".feedback"),
+    posts: document.querySelector(".posts"),
 
-    modal: document.querySelector('.modal'),
-    modalTitle: document.querySelector('.modal-title'),
-    modalBody: document.querySelector('.modal-body'),
-    modalLink: document.querySelector('.modal-link'),
-    localesBtnGroup: document.querySelector('.btn-group'),
-    feedsContainer: document.querySelector('.feeds'),
-    postsContainer: document.querySelector('.posts'),
+    modal: document.querySelector(".modal"),
+    modalTitle: document.querySelector(".modal-title"),
+    modalBody: document.querySelector(".modal-body"),
+    modalLink: document.querySelector(".modal-link"),
+    localesBtnGroup: document.querySelector(".btn-group"),
+    feedsContainer: document.querySelector(".feeds"),
+    postsContainer: document.querySelector(".posts"),
 
-    templateFeed: document.querySelector('#template-feeds-wrapper'),
-    templateFeedElement: document.querySelector('#template-feed-element'),
-    templatePost: document.querySelector('#template-posts-wrapper'),
-    templatePostElement: document.querySelector('#template-post-element'),
+    templateFeed: document.querySelector("#template-feeds-wrapper"),
+    templateFeedElement: document.querySelector("#template-feed-element"),
+    templatePost: document.querySelector("#template-posts-wrapper"),
+    templatePostElement: document.querySelector("#template-post-element"),
   };
 
   const initialState = {
     form: {
-      state: 'filling',
+      state: "filling",
       errors: null,
     },
-    lng: 'ru',
+    lng: "ru",
     urls: [],
     feeds: [],
     posts: [],
@@ -64,74 +67,89 @@ export default () => {
   });
 
   const watchedState = watch(initialState, elements, i18nextInstance);
-  elements.form.addEventListener('submit', (evt) => {
+  elements.form.addEventListener("submit", (evt) => {
     evt.preventDefault();
     const formData = new FormData(evt.target);
-    const url = formData.get('url');
+    const url = formData.get("url");
 
     validate(url, watchedState.urls)
       .then((urlRSS) => {
-        watchedState.form.errors = '';
-        watchedState.form.state = 'sending';
+        watchedState.form.errors = "";
+        watchedState.form.state = "sending";
         return fetchRSS(urlRSS);
       })
       .then((response) => {
         const data = parseRSS(response.data.contents);
+
+        data.feed.id = _.uniqueId();
+        data.feed.url = url;
         watchedState.feeds.unshift(data.feed);
+
         watchedState.posts = [...data.posts, ...watchedState.posts];
+        watchedState.posts.forEach((post) => {
+          post.id = _.uniqueId();
+          post.feedId = data.feed.id;
+        });
         watchedState.urls.push(url);
-        watchedState.form.errors = '';
-        watchedState.form.state = 'success';
-        console.log(watchedState);
+
+        watchedState.form.errors = "";
+        watchedState.form.state = "success";
       })
       .catch((err) => {
-        watchedState.form.state = 'failed';
+        watchedState.form.state = "failed";
         watchedState.form.errors = err.message;
       });
   });
 
-  elements.input.addEventListener('invalid', (evt) => {
+  elements.input.addEventListener("invalid", (evt) => {
     const validityState = evt.target.validity;
     if (validityState.valueMissing) {
-      evt.target.setCustomValidity(i18nextInstance.t('messages.emptyField'));
+      evt.target.setCustomValidity(i18nextInstance.t("messages.emptyField"));
     } else {
-      evt.target.setCustomValidity('');
+      evt.target.setCustomValidity("");
     }
   });
 
-  elements.posts.addEventListener('click', (evt) => {
-    if (evt.target.hasAttribute('data-id')) {
+  elements.posts.addEventListener("click", (evt) => {
+    if (evt.target.hasAttribute("data-id")) {
       const currentPostId = evt.target.dataset.id;
       watchedState.visitedPostsId.push(currentPostId);
     }
 
-    if (evt.target.hasAttribute('data-bs-toggle')) {
+    if (evt.target.hasAttribute("data-bs-toggle")) {
       const { id } = evt.target.dataset;
       watchedState.currentPost = watchedState.posts.find(
-        (post) => post.id === id,
+        (post) => post.id === id
       );
     }
   });
 
-  elements.localesBtnGroup.addEventListener('click', (evt) => {
+  elements.localesBtnGroup.addEventListener("click", (evt) => {
     const language = evt.target.dataset.lng;
     watchedState.lng = language;
   });
 
   const updatePosts = () => {
-    const promises = watchedState.urls.map((url) => {
+    const urls = watchedState.feeds.map((feed) => feed.url);
+    const promises = urls.map((url) => {
       fetchRSS(url)
         .then((response) => {
-          const updatedData = parseRSS(response.data.contents); // получили фид
-          const newPosts = updatedData.posts; // вытащили посты из фида
-          const postLinks = watchedState.posts.map((post) => post.link); // ссылки уже имеющихся постов
-          const addedPosts = newPosts.filter(
-            (post) => !postLinks.includes(post.link),
-          ); // отсеяли НОВЫЕ посты которых не было ранее
-          watchedState.posts = addedPosts.concat(...watchedState.posts); // склеили новые посты + имеющиеся
+          const data = parseRSS(response.data.contents);
+          const postsForFeed = watchedState.posts.filter(
+            (post) => post.feedId === data.feed.id
+          );
+          const postLinksForFeed = watchedState.posts
+            .filter((post) => post.id === data.feed.id)
+            .map((post) => post.link);
+          const addedPosts = postsForFeed.filter(
+            (post) => !postLinksForFeed.includes(post.link)
+          );
+          watchedState.posts = addedPosts.concat(...watchedState.posts);
           console.log(watchedState.posts);
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+          throw err
+        });
     });
 
     Promise.all(promises).finally(() => setTimeout(() => updatePosts(), 5000));
