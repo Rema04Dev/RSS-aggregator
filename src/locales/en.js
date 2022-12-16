2e5b3d65fdd26365fcc9cdae77b43ead6d7e5c0b
Rema04Dev <romansmerdyakov@gmail.com>
2022-12-17 00:12:44 +0600

    fix locales (en)

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,6 +1,6 @@
 export default {
   translation: {
-    title: 'RSS-agregator',
+    title: 'RSS-aggregator',
     subtitle: 'Receive notifications of new content in a convenient format.',
     placeholder: 'RSS link',
     example: 'Example https://ru.hexlet.io/lessons.rss',
