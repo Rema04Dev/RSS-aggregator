7f52975ceb0d4878dbcd623e39e93da0e7d79fc7
Rema04Dev <romansmerdyakov@gmail.com>
2022-12-17 14:52:53 +0600

    fix magic numbers

--- src/app.js
+++ src/app.js
@@ -1,57 +1,56 @@
-import * as yup from "yup";
-import i18next from "i18next";
-import axios from "axios";
-import _ from "lodash";
-import watch from "./view";
-import resources from "./locales/index";
-import parseRSS from "./parseRSS";
+import * as yup from 'yup';
+import i18next from 'i18next';
+import axios from 'axios';
+import _ from 'lodash';
+import watch from './view';
+import resources from './locales/index';
+import parseRSS from './parseRSS';
 
-const defaulltLng = "ru";
+const DEFAULT_LANGUAGE = 'ru';
+const DELAY = 5000;
 
-const validate = (url, urls) =>
-  yup
-    .string()
-    .required()
-    .url("mustBeValid")
-    .notOneOf(urls, "linkExists")
-    .validate(url);
+const validate = (url, urls) => yup
+  .string()
+  .required()
+  .url('mustBeValid')
+  .notOneOf(urls, 'linkExists')
+  .validate(url);
 
-const buildProxyURL = (url) =>
-  `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(
-    url
-  )}`;
+const buildProxyURL = (url) => `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(
+  url,
+)}`;
 
 const fetchRSS = (url) => axios.get(buildProxyURL(url));
 
 export default () => {
   const elements = {
-    form: document.querySelector(".rss-form"),
-    input: document.querySelector("#url-input"),
+    form: document.querySelector('.rss-form'),
+    input: document.querySelector('#url-input'),
     button: document.querySelector('[aria-label="add"]'),
 
-    feedback: document.querySelector(".feedback"),
-    posts: document.querySelector(".posts"),
+    feedback: document.querySelector('.feedback'),
+    posts: document.querySelector('.posts'),
 
-    modal: document.querySelector(".modal"),
-    modalTitle: document.querySelector(".modal-title"),
-    modalBody: document.querySelector(".modal-body"),
-    modalLink: document.querySelector(".modal-link"),
-    localesBtnGroup: document.querySelector(".btn-group"),
-    feedsContainer: document.querySelector(".feeds"),
-    postsContainer: document.querySelector(".posts"),
+    modal: document.querySelector('.modal'),
+    modalTitle: document.querySelector('.modal-title'),
+    modalBody: document.querySelector('.modal-body'),
+    modalLink: document.querySelector('.modal-link'),
+    localesBtnGroup: document.querySelector('.btn-group'),
+    feedsContainer: document.querySelector('.feeds'),
+    postsContainer: document.querySelector('.posts'),
 
-    templateFeed: document.querySelector("#template-feeds-wrapper"),
-    templateFeedElement: document.querySelector("#template-feed-element"),
-    templatePost: document.querySelector("#template-posts-wrapper"),
-    templatePostElement: document.querySelector("#template-post-element"),
+    templateFeed: document.querySelector('#template-feeds-wrapper'),
+    templateFeedElement: document.querySelector('#template-feed-element'),
+    templatePost: document.querySelector('#template-posts-wrapper'),
+    templatePostElement: document.querySelector('#template-post-element'),
   };
 
   const initialState = {
     form: {
-      state: "filling",
+      state: 'filling',
       errors: null,
     },
-    lng: "ru",
+    lng: 'ru',
     feeds: [],
     posts: [],
     visitedPostsId: [],
@@ -60,22 +59,21 @@ export default () => {
 
   const i18nextInstance = i18next.createInstance();
   i18nextInstance.init({
-    lng: defaulltLng,
+    lng: DEFAULT_LANGUAGE,
     debug: false,
     resources,
   });
 
   const watchedState = watch(initialState, elements, i18nextInstance);
-  elements.form.addEventListener("submit", (evt) => {
+  elements.form.addEventListener('submit', (evt) => {
     evt.preventDefault();
     const formData = new FormData(evt.target);
-    const url = formData.get("url");
+    const url = formData.get('url');
     const urls = watchedState.feeds.map((feed) => feed.url);
-    /* eslint no-param-reassign: ["error", { "props": false }] */
     validate(url, urls)
       .then((urlRSS) => {
-        watchedState.form.errors = "";
-        watchedState.form.state = "sending";
+        watchedState.form.errors = '';
+        watchedState.form.state = 'sending';
         return fetchRSS(urlRSS);
       })
       .then((response) => {
@@ -86,73 +84,73 @@ export default () => {
         watchedState.feeds.unshift(data.feed);
 
         watchedState.posts = [...data.posts, ...watchedState.posts];
-        watchedState.posts.forEach((post) => {
+        watchedState.posts.forEach((p) => {
+          const post = p;
           post.id = _.uniqueId();
           post.feedId = data.feed.id;
         });
-        watchedState.form.errors = "";
-        watchedState.form.state = "success";
+        watchedState.form.errors = '';
+        watchedState.form.state = 'success';
       })
       .catch((err) => {
-        if (err.message === "Network Error") {
-          watchedState.form.errors = "network";
+        if (err.message === 'Network Error') {
+          watchedState.form.errors = 'network';
         } else {
           watchedState.form.errors = err.message;
         }
       });
   });
 
-  elements.input.addEventListener("invalid", (evt) => {
+  elements.input.addEventListener('invalid', (evt) => {
     const validityState = evt.target.validity;
     if (validityState.valueMissing) {
-      evt.target.setCustomValidity(i18nextInstance.t("messages.emptyField"));
+      evt.target.setCustomValidity(i18nextInstance.t('messages.emptyField'));
     } else {
-      evt.target.setCustomValidity("");
+      evt.target.setCustomValidity('');
     }
   });
 
-  elements.posts.addEventListener("click", (evt) => {
-    if (evt.target.hasAttribute("data-id")) {
+  elements.posts.addEventListener('click', (evt) => {
+    if (evt.target.hasAttribute('data-id')) {
       const currentPostId = evt.target.dataset.id;
       watchedState.visitedPostsId.push(currentPostId);
     }
 
-    if (evt.target.hasAttribute("data-bs-toggle")) {
+    if (evt.target.hasAttribute('data-bs-toggle')) {
       const { id } = evt.target.dataset;
       watchedState.currentPost = watchedState.posts.find(
-        (post) => post.id === id
+        (post) => post.id === id,
       );
     }
   });
 
-  elements.localesBtnGroup.addEventListener("click", (evt) => {
+  elements.localesBtnGroup.addEventListener('click', (evt) => {
     const language = evt.target.dataset.lng;
     watchedState.lng = language;
   });
 
   const updatePosts = () => {
     const urls = watchedState.feeds.map((feed) => feed.url);
-    const promises = urls.map((url) =>
-      fetchRSS(url)
-        .then((response) => {
-          const data = parseRSS(response.data.contents);
-          const postsForFeed = watchedState.posts.filter(
-            (post) => post.feedId === data.feed.id
-          );
-          const postLinksForFeed = watchedState.posts
-            .filter((post) => post.id === data.feed.id)
-            .map((post) => post.link);
-          const addedPosts = postsForFeed.filter(
-            (post) => !postLinksForFeed.includes(post.link)
-          );
-          watchedState.posts = addedPosts.concat(...watchedState.posts);
-        })
-        .catch((err) => {
-          throw err;
-        })
-    );
+    const promises = urls.map((url) => fetchRSS(url)
+      .then((response) => {
+        const data = parseRSS(response.data.contents);
+        const postsForFeed = watchedState.posts.filter(
+          (post) => post.feedId === data.feed.id,
+        );
+        const postLinksForFeed = watchedState.posts
+          .filter((post) => post.id === data.feed.id)
+          .map((post) => post.link);
+        const addedPosts = postsForFeed.filter(
+          (post) => !postLinksForFeed.includes(post.link),
+        );
+        watchedState.posts = addedPosts.concat(...watchedState.posts);
+        console.log(watchedState.posts);
+      })
+      .catch((err) => {
+        throw err;
+      }));
 
-    Promise.all(promises).finally(() => setTimeout(() => updatePosts(), 5000));
+    Promise.all(promises).finally(() => setTimeout(() => updatePosts(), DELAY));
   };
-  updatePosts();
+  setTimeout(updatePosts, DELAY);
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.js";
-import "./styles/main.scss";
-import app from "./app.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import './styles/main.scss';
+import app from './app.js';
 
 app();
